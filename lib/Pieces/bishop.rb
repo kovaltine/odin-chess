1ed2c25d0677042cc8177dfb23c1e6f47fb845d7
koka <kiraoverton27@gmail.com>
2021-10-09 15:25:16 -0400

    bishop class file added

---
+++ lib/Pieces/bishop.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal:true
+
+# determines the behaviour of bishop pieces
+class Bishop
+  def initialize(surrounding, square)
+    @start = square
+    @surrounding = surrounding
+  end
+
+  def bishop_pattern
+    pos_y_pos_x = diagonal_pattern([1, 1])
+    pos_y_pos_x_options = limit_axis_options(pos_y_pos_x)
+
+    neg_y_pos_x = diagonal_pattern([-1, 1])
+    neg_y_pos_x_options = limit_axis_options(neg_y_pos_x)
+
+    pos_y_neg_x = diagonal_pattern([1, -1])
+    pos_y_neg_x_options = limit_axis_options(pos_y_neg_x)
+
+    neg_y_neg_x = diagonal_pattern([-1, -1])
+    neg_y_neg_x_options = limit_axis_options(neg_y_neg_x)
+
+    [pos_y_pos_x_options, neg_y_pos_x_options, pos_y_neg_x_options, neg_y_neg_x_options].flatten(1)
+  end
+
+  def diagonal_pattern(pattern, coord = @start.dup, bishop_line = [])
+    return bishop_line unless (coord[0] + pattern[0]).between?(0, 7) && (coord[1] + pattern[1]).between?(0, 7)
+
+    bishop_line.push([coord[0] += pattern[0], coord[1] += pattern[1]])
+
+    diagonal_pattern(pattern, coord, bishop_line)
+  end
+
+  def limit_axis_options(arr)
+    return nil if arr.nil?
+
+    options = []
+    arr.each do |option|
+      options.push(option)
+      return options if @surrounding.include?(option)
+    end
+  end
+end
